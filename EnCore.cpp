e2cc01b8283487b005cad5486b302190fe5ddbbf
Bjathneas <bjathneas@gmail.com>
2023-04-29 10:08:19 -0500

    Changed Manager::entities to unordered_map for better performance

--- EnCore.cpp
+++ EnCore.cpp
@@ -8,22 +8,23 @@ unsigned int Entity::get_uid() { return uid; }
 Manager::Manager() {}
 
 unsigned int Manager::create_entity() {
-  entities.push_back(new Entity(next_entity_id));
+  entities[next_entity_id] = new Entity(next_entity_id);
   next_entity_id++;
   return next_entity_id - 1;
 }
 
 void Manager::destroy_entity(unsigned int uid) {
-  auto entity = std::remove(entities.begin(), entities.end(), get_entity(uid));
-  std::for_each(entities.begin(), entity,
-                [](Entity* entity) { delete entity; });
-  entities.erase(entity, entities.end());
+  auto it = entities.find(uid);
+  if (it != entities.end()) {
+    delete entities[uid];
+    entities.erase(it);
+  }
 }
 
 Entity* Manager::get_entity(unsigned int uid) {
-  for (Entity* entity : entities) {
-    if (entity->get_uid() == uid) return entity;
-  }
+  auto it = entities.find(uid);
+  if(it != entities.end())
+    return entities[uid];
   return nullptr;
 }
 
