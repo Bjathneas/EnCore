51de386ba25c7bda02b95e75a6b808b179f6d260
Bjathneas <bjathneas@gmail.com>
2023-04-30 18:53:44 -0500

    Made Header only

--- EnCore.h
+++ EnCore.h
@@ -21,9 +21,9 @@ struct Component {};
 
 class Entity {
  public:
-  Entity(unsigned int uid);
+  inline Entity(unsigned int uid) : uid(uid) {}
 
-  unsigned int get_uid();
+  inline unsigned int get_uid() { return uid; }
 
   template <typename Comp>
   void add_component(std::shared_ptr<Comp> component) {
@@ -62,19 +62,33 @@ class Entity {
 
 class Manager {
  public:
-  Manager();
+  Manager() {}
 
-  unsigned int create_entity();
-  void destroy_entity(unsigned int uid);
+  inline unsigned int create_entity() {
+    entities[next_entity_id] = new Entity(next_entity_id);
+    next_entity_id++;
+    return next_entity_id - 1;
+  }
+  inline void destroy_entity(unsigned int uid) {
+    auto it = entities.find(uid);
+    if (it != entities.end()) {
+      delete entities[uid];
+      entities.erase(it);
+    }
+  }
 
-  Entity* get_entity(unsigned int uid);
+  Entity* get_entity(unsigned int uid) {
+    auto it = entities.find(uid);
+    if (it != entities.end()) return entities[uid];
+    return nullptr;
+  }
 
   template <typename Comp>
   std::vector<Entity*> get_entities_with_components() {
     std::vector<Entity*> entities_with_components;
 
     for (std::pair<unsigned int, Entity*> pair : entities) {
-     Entity* entity = std::get<1>(pair);
+      Entity* entity = std::get<1>(pair);
       if (entity->contains<Comp>()) entities_with_components.push_back(entity);
     }
 
@@ -82,14 +96,13 @@ class Manager {
   }
 
  private:
-  // @TODO Make entities into a hashmap
   std::unordered_map<unsigned int, Entity*> entities;
   unsigned int next_entity_id{0};
 };
 
 class System {
  public:
-  System();
+  System() {}
 
   virtual void update(Manager manager) = 0;
 };
